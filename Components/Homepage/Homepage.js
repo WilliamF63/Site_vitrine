c3e24c7b0af5aded178a0d97853361ab69c3b531
63Paulo <fernandespaul548@gmail.com>
2023-06-06 17:04:07 +0200

    Homepage - Diaporama

--- Components/Homepage/Homepage.js
+++ Components/Homepage/Homepage.js
@@ -1,5 +1,14 @@
-import Diaporama from '../Homepage/Diaporama.js'
-import ButtonPrendreRDV from '../Homepage/BoutonPrendreRDV.js'
+import Diaporama from '../Homepage/Diaporama.js';
+import ButtonPrendreRDV from '../Homepage/BoutonPrendreRDV.js';
+
+import image1 from '/public/images/diapo/dt-img.jpg';
+import image2 from '/public/images/diapo/dt-img2.jpg';
+import image3 from '/public/images/diapo/dt-img3.jpg';
+import image4 from '/public/images/diapo/dt-img4.jpg';
+import image5 from '/public/images/diapo/dt-img5.jpg';
+
+
+const images = [image1,image2, image3, image4, image5 ];
 
 const Homepage = () => {
 
@@ -8,8 +17,8 @@ const Homepage = () => {
          <section className="">
           <h1 className="text-6xl uppercase text-center mt-16 pt-16 relative">
             Bienvenue au cabinet dentaire</h1>
+            <Diaporama images={images}/>
           </section>
-          <Diaporama/>
        </>
     
       );
