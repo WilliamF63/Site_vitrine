58a130c9ab31c6f6b3e7406362c9a1c2164ab52d
63Paulo <fernandespaul548@gmail.com>
2023-06-06 15:56:26 +0200

    Site Vitrine

---
+++ Components/Homepage/Diaporama.js
@@ -0,0 +1,16 @@
+
+
+const Diaporama = () => {
+
+      return (
+        <>
+         <section className="">
+          <h1 className="text-6xl uppercase text-center mt-16 pt-16 relative">
+            Bienvenue au cabinet dentaire</h1>
+          </section>
+       </>
+    
+      );
+    };
+    
+export default Diaporama;
--- Components/Homepage/Homepage.js
+++ Components/Homepage/Homepage.js
@@ -5,8 +5,9 @@ const Homepage = () => {
 
       return (
         <>
-          <section className=''>
-            <h1 className='text-5xl uppercase text-center mt-16'>Bienvenue au cabinet dentaire</h1>
+         <section className="">
+          <h1 className="text-6xl uppercase text-center mt-16 pt-16 relative">
+            Bienvenue au cabinet dentaire</h1>
           </section>
        </>
     
