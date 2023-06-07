a760548c05ce9369b1e833d7927f3b53a734c1f0
63Paulo <fernandespaul548@gmail.com>
2023-06-07 15:50:35 +0200

    Footer

--- Components/Homepage/BoutonPrendreRDV.js
+++ Components/Homepage/BoutonPrendreRDV.js
@@ -1,7 +1,7 @@
 const BoutonPrendreRDV = () => {
 
     return (
-        <a href='https://www.doctolib.fr/' className='uppercase w-48 text-center text-2xl mt-14 mb-12 bg-cyan-950 text-white p-2 flex justify-center items-center
+        <a href='https://www.doctolib.fr/' className='uppercase w-48 text-center text-2xl mt-14 mb-16 bg-cyan-950 text-white p-2 flex justify-center items-center
       mx-auto border rounded-lg hover:bg-cyan-900'>
         Prendre RDV
       </a>
--- Components/NavBar/NavBar.js
+++ Components/NavBar/NavBar.js
@@ -2,33 +2,33 @@ import Image from "next/image";
 
 const NavBar = () => {
     return (
-        <div className="w-full max-w-full">
-
-        <nav className="bg-gray-800">
-            <div className="ml-3">
-                <div className="flex items-center md:justify-between h-16">
-                    <div className="flex-shrink-0">
-                        <a href="/">
-                            <Image 
-                                src="/../public/images/logo/logo-dentiste.png"
-                                alt="Logo"
-                                width={40}
-                                height={40}
-                            />
-                        </a>
+        
+            <div className="w-full">
+                <nav className="bg-gray-800">
+                    <div className="ml-3">
+                        <div className="flex items-center sm:justify-between h-16">
+                            <div className="flex-shrink-0">
+                                <a href="/">
+                                    <Image 
+                                        src="/../public/images/logo/logo-dentiste.png"
+                                        alt="Logo"
+                                        width={40}
+                                        height={40}
+                                    />
+                                </a>
+                            </div>
+                            <div className="">
+                                <div className="ml-10 flex items-center">
+                                    <a href="/" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Accueil</a>
+                                    <a href="/" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Cabinet</a>
+                                    <a href="/" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Equipements</a>
+                                    <a href="/" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Contact</a>
+                                    <a href='https://www.doctolib.fr/' className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Prendre RDV</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="">
-                        <div className="ml-10 flex items-center">
-                            <a href="/" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Accueil</a>
-                            <a href="/" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Cabinet</a>
-                            <a href="/" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Equipements</a>
-                            <a href="/" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Contact</a>
-                            <a href='https://www.doctolib.fr/' className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Prendre RDV</a>
-                         </div>
-                    </div>
-                </div>
-            </div>
-        </nav>
+                </nav>
         </div>
 
     )
