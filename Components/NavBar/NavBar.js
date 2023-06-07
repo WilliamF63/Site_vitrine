2e8b7bb1a6edeb6df3fd4693c552e51231842b77
63Paulo <fernandespaul548@gmail.com>
2023-06-07 14:19:54 +0200

    Navbar

--- Components/NavBar/NavBar.js
+++ Components/NavBar/NavBar.js
@@ -2,9 +2,11 @@ import Image from "next/image";
 
 const NavBar = () => {
     return (
+        <div className="w-full max-w-full">
+
         <nav className="bg-gray-800">
-            <div className="max-w-3xl mx-auto px-4 md:max-w-7xl">
-                <div className="flex items-center justify-between h-16">
+            <div className="ml-3">
+                <div className="flex items-center md:justify-between h-16">
                     <div className="flex-shrink-0">
                         <a href="/">
                             <Image 
@@ -15,18 +17,19 @@ const NavBar = () => {
                             />
                         </a>
                     </div>
-      <div className=" md:block">
-        <div className="ml-10 flex items-baseline space-x-4">
-          <a href="/" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Accueil</a>
-          <a href="/" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Cabinet</a>
-          <a href="/" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Equipements</a>
-          <a href="/" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Contact</a>
-          <a href='https://www.doctolib.fr/' className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Prendre RDV</a>
+                    <div className="">
+                        <div className="ml-10 flex items-center">
+                            <a href="/" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Accueil</a>
+                            <a href="/" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Cabinet</a>
+                            <a href="/" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Equipements</a>
+                            <a href="/" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Contact</a>
+                            <a href='https://www.doctolib.fr/' className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Prendre RDV</a>
+                         </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
         </div>
-      </div>
-    </div>
-  </div>
-</nav>
 
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,6 @@ export default function Home() {
     <>
       <Head>
         <title>CABINET DENTAIRE</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <NavBar/>
       <Homepage/>
