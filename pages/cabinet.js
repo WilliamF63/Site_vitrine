1b2342cba55bfff646f1ffbe69aebf9cd51efaee
63Paulo <fernandespaul548@gmail.com>
2023-06-08 11:49:12 +0200

    Site multipages

--- pages/cabinet.js
+++ pages/cabinet.js
@@ -1,7 +1,16 @@
-export default function Cabinet() {
-    return (
-        <>
-            <Cabinet/>
-        </>
-    )
-}
+import React from 'react';
+import Cabinet from '../Components/Cabinet/Cabinet';
+import Footer from '../Components/Footer/Footer';
+import NavBar from '../Components/NavBar/NavBar';
+
+const CabinetPage = () => {
+  return (
+    <div>
+      <NavBar/>
+      <Cabinet />
+      <Footer/>
+    </div>
+  );
+};
+
+export default CabinetPage;
---
+++ pages/homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Homepage from '../Components/Homepage/Homepage';
+import NavBar from '../Components/NavBar/NavBar';
+
+const HomepagePage = () => {
+  return (
+    <div>
+      <NavBar/>
+      <Homepage />
+    </div>
+  );
+};
+
+export default HomepagePage;
