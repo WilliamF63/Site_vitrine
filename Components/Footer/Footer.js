ea139d68c63d60e72943d6c8253e015f7f4f9c91
63Paulo <fernandespaul548@gmail.com>
2023-06-08 11:36:56 +0200

    site multipages

--- Components/Footer/Footer.js
+++ Components/Footer/Footer.js
@@ -17,10 +17,10 @@ const Footer = () => {
             <div>
               <h3 className="text-lg font-bold">Plan du site</h3>
               <ul className="mt-4 space-y-2">
-                <li><a className="hover:underline" href="/">Acceuil</a></li>
-                <li><a className="hover:underline" href="/">Cabinet</a></li>
-                <li><a className="hover:underline" href="/">Equipements</a></li>
-                <li><a className="hover:underline" href="/">Contact</a></li>
+                <li><a className="hover:underline" href="/homepage">Acceuil</a></li>
+                <li><a className="hover:underline" href="/cabinet">Cabinet</a></li>
+                <li><a className="hover:underline" href="/equipements">Equipements</a></li>
+                <li><a className="hover:underline" href="/contact">Contact</a></li>
               </ul>
             </div>
             <div>
--- Components/NavBar/NavBar.js
+++ Components/NavBar/NavBar.js
@@ -19,10 +19,10 @@ const NavBar = () => {
                             </div>
                             <div className="">
                                 <div className="ml-10 flex items-center">
-                                    <a href="/Homepage" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Accueil</a>
-                                    <a href="/Cabinet" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Cabinet</a>
-                                    <a href="/" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Equipements</a>
-                                    <a href="/" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Contact</a>
+                                    <a href="/homepage" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Accueil</a>
+                                    <a href="/cabinet" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Cabinet</a>
+                                    <a href="/equipements" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Equipements</a>
+                                    <a href="/contact" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Contact</a>
                                     <a href='https://www.doctolib.fr/' target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white hover:underline px-3 py-2 rounded-md text-sm font-medium">Prendre RDV</a>
                                 </div>
                             </div>
