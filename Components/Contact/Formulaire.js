ba3fa1ce0bd042c64e99c21c70cefc47dccb4317
63Paulo <fernandespaul548@gmail.com>
2023-06-09 11:12:26 +0200

    Contact - Formulaire

--- Components/Contact/Formulaire.js
+++ Components/Contact/Formulaire.js
@@ -1,27 +1,27 @@
 const Formulaire = () => {
     return (
-        <div className="bg-gray-600 mb-16 flex justify-center text-white ">
+        <div className="bg-gray-700 mb-16 flex justify-center text-white ">
 
             <form className="flex items-center flex-col w-1/2 mt-6">
             <div className="mb-4 flex w-full">
                     <div className="mr-2 w-full">
                         <label htmlFor="firstName" className="block font-bold mb-1">Prénom</label>
-                        <input type="text" id="firstName" className="w-full px-3 py-2 border border-gray-300 rounded" />
+                        <input type="text" id="firstName" className="w-full px-3 py-2 border border-gray-300 rounded text-black" />
                     </div>
                     <div className="w-full">
                         <label htmlFor="lastName" className="block font-bold mb-1">Nom</label>
-                        <input type="text" id="lastName" className="w-full px-3 py-2 border border-gray-300 rounded" />
+                        <input type="text" id="lastName" className="w-full px-3 py-2 border border-gray-300 rounded text-black" />
                     </div>
                 </div>
                 <div className="mb-4 w-full">
                     <label htmlFor="subject" className="block font-bold mb-1">Objet</label>
-                    <input type="text" id="subject" className="w-full px-3 py-2 border border-gray-300 rounded" />
+                    <input type="text" id="subject" className="w-full px-3 py-2 border border-gray-300 rounded text-black" />
                 </div>
                 <div className="mb-4 w-full h-full">
                     <label htmlFor="message" className="block font-bold  mb-1">Message</label>
-                    <textarea id="message" className="w-full h-full px-3 py-2 border border-gray-300 rounded" rows="4"></textarea>
+                    <textarea id="message" className="w-full h-full px-3 py-2 border border-gray-300 rounded text-black" rows="4"></textarea>
                 </div>
-                <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-12">
+                <button type="submit" className="bg-cyan-900 hover:bg-cyan-950 text-white font-bold py-2 px-4 rounded my-12">
                     Envoyer
                 </button>
             </form>
