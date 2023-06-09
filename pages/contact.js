ad9e49f3cc8669f3f3a9ec05de5a41c3e5eb6056
63Paulo <fernandespaul548@gmail.com>
2023-06-09 10:41:02 +0200

    Début page contact

--- pages/contact.js
+++ pages/contact.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Footer from '../Components/Footer/Footer';
 import NavBar from '../Components/NavBar/NavBar';
+import Contact from '../Components/Contact/Contact';
 
 const ContactPage = () => {
   return (
     <div>
       <NavBar/>
-
+      <Contact/>
       <Footer/>
     </div>
   );
